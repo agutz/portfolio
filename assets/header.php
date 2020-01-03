87bb7c82a9fdc36bb997d0a65c10ea6ba5c2fb2d
Adrian Gutz <38659306+agutz@users.noreply.github.com>
2020-01-03 01:23:59 -0400

    Add files via upload

---
+++ assets/header.php
@@ -0,0 +1,52 @@
+<header>
+<div class="logo-box">
+<a class="logo animated bounceIn" href="/portfolio"><img alt="" src="images/logo.png"></a>
+</div>
+
+<div class="info-wrap">
+<h1>Hi I'm <span class="text-info">Adrian</span> Gutierrez</h1>
+<h6 class="text-info">Web App Developer - UI / UX Designer</h6>
+
+<p>My passion completely dedicated to the UI effects, animations and providing intuitive and dynamic interfaces for the best user experience.</p>
+<!-- 
+<h1 class="text-info">...Thank you for interest in my Creative Services</h1> -->
+
+<!-- <p class="text-clear">I create fresh and precise web interfaces focused on the best user experience with a
+strong technical background and self-discipline to put attention to details, able to play a
+key role throughout a sites development to ensure maximum accessibility and usability.</p>
+
+<p class="text-clear">A quick learner who can absorb new ideas and can communicate clearly and
+effectively to produce detailed technical specifications from client requirements.</p>
+<p class="text-info">Currently looking for an opportunity to be part of new creative and potential projects.</p> -->
+
+</div>
+
+<a class="elemadded responsive-link" href="#">Menu</a>
+
+<div class="menu-box">
+<ul class="menu">
+
+<li><a href="/portfolio"  <?php if ($menuselected == 1){?> class="active" <?php }?>><span>Recent Work</span></a></li>
+<li><a href="skills.php" <?php if ($menuselected == 2){?> class="active" <?php }?>><span>Skills / Experience</span></a></li>
+<li><a href="contact.php" <?php if ($menuselected == 3){?> class="active" <?php }?>><span>Contact</span></a></li>
+</ul>				
+</div>
+
+<!-- <div class="filter-box">
+<ul class="filter">
+<li><a href="#" class="active" data-filter="*">All Projects</a></li>
+<li><a href="#" data-filter=".web-design">Web Design</a></li>
+<li><a href="#" data-filter=".graphics-design">Graphics Design</a></li>
+<li><a href="#" data-filter=".logo">Logos</a></li>
+<li><a href="#" data-filter=".games">Games & Apps</a></li>
+</ul>
+</div>-->
+
+
+
+<!-- <div class="social-box"> 
+<p class="copyright">&#169; 2019</p>
+</div> -->
+
+
+</header>
---
+++ assets/info-box.php
@@ -0,0 +1,10 @@
+<!--<div class="info-box">
+<a class="info-toggle" href="#"><img src="images/info.png" alt="Info"/></a>
+<div class="info-content">Email: <a href="mail:info@agutz.com" style="color:#FFF;">info@agutz.com</a>
+</div>
+</div>
+
+
+<div class="preloader">
+<img alt="" src="images/preloader.gif">
+</div>-->
