9c0d18bfb4b857c6ae40e38130e3bff3ba6d04e4
Adrian Gutz <38659306+agutz@users.noreply.github.com>
2020-01-03 01:16:50 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,512 @@
+<!doctype html>
+
+
+<html lang="en" class="no-js">
+<head>
+<title>Agutz | Web Development & Graphic Design</title>
+<meta name="description" content="Web Development & Graphic Design Freelance">
+<meta name="keywords" content="Web Developer & Multi Creative Designer">
+
+<meta charset="utf-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<script type="text/javascript">
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-33738064-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+</script>
+
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" media="screen">	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/flexslider.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/animate.css" media="screen">
+<link rel="stylesheet" type="text/css" href="css/responsive.css" media="screen">
+
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/ico/logo-144.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/ico/logo-144.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/ico/logo-72.png">
+<link rel="apple-touch-icon-precomposed" href="images/ico/logo-57.png">
+<link rel="shortcut icon" href="images/ico/favicon.png">
+
+<?php $menuselected = 1;?>
+</head>
+
+<body>
+<!-- body oncontextmenu="return false;" -->
+
+<!-- Container -->
+<div id="container">
+<!-- Header
+================================================== -->
+<!-- End Header -->
+
+<?php include ('assets/header.php');?>
+
+<!-- content 
+================================================== -->
+<div id="content">
+<div class="inner-content">
+
+
+<div class="portfolio-page">
+<div class="portfolio-box">
+
+
+
+<div class="project-post">
+<img alt="" src="upload/wso.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Wine Stuff Online</h2>
+<span>eCommerce Development (Woocomerce)</span>
+<div><a class="icon-action zoom" href="upload/wso-lg.jpg" title="Wine Stuff Online / eCommerce Development (Woocomerce)"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.winestuffonline.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+<div class="project-post">
+<img alt="" src="upload/ptms.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Pass Thru Merchant Services</h2>
+<span>Web Development, Brand Design</span>
+<div><a class="icon-action zoom" href="upload/ptms-lg.jpg" title="Pass Thru Merchant Services / Web Development, Brand Design"><i class="fa fa-search"></i></a> <a class="icon-action" href="https://www.passthrumerchantservices.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+<div class="project-post">
+<img alt="" src="upload/dji-sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>DJI Drones RD</h2>
+<span>Web Development</span>
+<div><a class="icon-action zoom" href="upload/dji-lg.jpg" title="DJI Drones RD / Web Development"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.djidronesrd.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="project-post">
+<img alt="" src="upload/foodboard_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Foodboard</h2>
+<span>Web Application Development / Restaurant Sales Manager</span>
+<div><a class="icon-action zoom" href="upload/foodboard.jpg" title="Foodboard / Restaurant Sales Manager"><i class="fa fa-search"></i></a> </div>
+</div>
+</div>
+</div>
+
+<!-- <a class="icon-action" href="http://foodboard.gooddeveloper.info/" target="_blank"><i class="fa fa-link"></i></a> -->
+
+
+<div class="project-post">
+<img alt="" src="upload/werteachers-sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>WeRTeachers</h2>
+<span>UI / UX Design & Development</span>
+<div><a class="icon-action zoom" href="upload/werteachers-lg.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="wrt/_site" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+<div class="project-post">
+<img alt="" src="upload/image1.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Orologio Jewelry</h2>
+<span>Brand Design</span>
+<div><a class="icon-action zoom" href="upload/orologio.jpg" title="Orologio Jewelry / Brand Design"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+
+<div class="project-post">
+<img alt="" src="upload/boost_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Boost Web Seo</h2>
+<span>Logotype + Website Design and Development</span>
+<div><a class="icon-action zoom" href="upload/boost_lg.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.boostwebseo.com" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+<div class="project-post">
+<img alt="" src="upload/factboard_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Factboard</h2>
+<span>Web Application Development / Accounting Application</span>
+<div><a class="icon-action zoom" href="upload/factboard.jpg" title="Factboard / Accounting Application"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>
+
+
+<!--  <a class="icon-action" href="http://factboard.gooddeveloper.info/" target="_blank"><i class="fa fa-link"></i></a> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="project-post">
+<img alt="" src="upload/one_republic_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>One Republic</h2>
+<span>Brand Design</span>
+<div><a class="icon-action zoom" href="upload/one_republic.jpg" title="One Republic / Brand Design"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+<div class="project-post">
+<img alt="" src="upload/bridging-medical-access.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Bridging Medical Access</h2>
+<span>Brand + Web Development</span>
+<div><a class="icon-action zoom" href="upload/bridging-medical-access-lg.jpg" title="Bridging Medical Access / Brand + Web Development"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.bridgingmedicalaccess.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+<div class="project-post">
+<img alt="" src="upload/sdt_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Solutions Dental Team</h2>
+<span>Brand Design / Web Development (WP)</span>
+<div><a class="icon-action zoom" href="upload/sdt.jpg" title="Solutions Dental Team / Brand Design / Web Development"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.solutionsdentalteam.com/site" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+<div class="project-post">
+<img alt="" src="upload/eps.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>EPS Renewables</h2>
+<span>Web Development (Wordpress)</span>
+<div><a class="icon-action zoom" href="upload/eps-large.jpg" title="EPS Renewables / Web Development (Wordpress)"><i class="fa fa-search"></i></a><a class="icon-action" href="http://www.epsrenewables.com" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<!-- <div class="project-post">
+<img alt="" src="upload/vedom_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>VEDOM SRL</h2>
+<span>Brand Design / Web Development</span>
+<div><a class="icon-action zoom" href="upload/vedom.jpg" title="VEDOM, SRL / Brand Design / Web Development"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.vedomsrl.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> -->
+
+
+
+
+
+
+<!-- <div class="project-post">
+<img alt="" src="upload/gce-sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Gold Crafters Exchange</h2>
+<span>Responsive Website Development + Design </span>
+<div><a class="icon-action zoom" href="upload/gce.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.goldcraftersexchange.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> -->
+
+
+
+<!--  <div class="project-post web-design">
+<img alt="" src="upload/image9.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Warren Exchange</h2>
+<span>Responsive Web Design + Development</span>
+<div><a class="icon-action" href="http://www.warrenexchange.com" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> 
+
+ <div class="project-post">
+<img alt="" src="upload/fmb.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Finance My Boat</h2>
+<span>Responsive Website Development</span>
+<div><a class="icon-action zoom" href="upload/fmb_lg.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.financemyboat.com" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> 
+
+
+
+
+<div class="project-post">
+<img alt="" src="upload/drbma.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Dr.BMA</h2>
+<span>Logotype + UI Design Website and Dashboards</span>
+<div><a class="icon-action zoom" href="upload/drbma-lg.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.drbma.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> -->
+
+
+
+<!-- 
+<div class="project-post">
+<img alt="" src="upload/vbtechnologies-sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>V&B Techonlogies, LLC</h2>
+<span>Logotype Design</span>
+<div><a class="icon-action zoom" href="upload/vbtechnologies-lg.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://vbtechnologiesllc.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div> -->
+
+
+ 
+
+ 
+
+
+
+<!--<div class="project-post">
+<img alt="" src="upload/image4.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Você Club</h2>
+<span>Shoes Catalog Design</span>
+<div><a class="icon-action zoom" href="upload/image4.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>-->
+
+<!-- <div class="project-post logo web-design ">
+<img alt="" src="upload/ime.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>IME Help</h2>
+<span>Logotype Design + Responsive Website Development</span>
+<div><a class="icon-action zoom" href="upload/ime-large.jpg"><i class="fa fa-search"></i></a> </div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post logo graphics-design">
+<img alt="" src="upload/oldranch_sm.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Old Ranch Cigars Most Wanted</h2>
+<span>Logotype + Foil Wraps, Boxes</span>
+<div><a class="icon-action zoom" href="upload/oldranch_lg.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>-->
+
+
+<!--<div class="project-post logo graphics-design">
+<img alt="" src="upload/guasabara.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Gusabara Team</h2>
+<span>Logotype Design</span>
+<div><a class="icon-action zoom" href="upload/guasabara-large.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>
+
+<div class="project-post logo web-design ">
+<img alt="" src="upload/rare_pawn.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Rare Pawn</h2>
+<span>Logotype Design + eCommerce Development</span>
+<div><a class="icon-action zoom" href="upload/rare_pawn_lg.jpg"><i class="fa fa-search"></i></a> </div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post web-design">
+<img alt="" src="upload/bma.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Better Medical Advice</h2>
+<span>Concept & Website Design</span>
+<div><a class="icon-action" href="upload/bma-large.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.bettermedicaladvice.com" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post web-design">
+<img alt="" src="upload/esp.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Eagle Star Petroleum</h2>
+<span>Website Design</span>
+<div><a class="icon-action" href="upload/esp-large.jpg"><i class="fa fa-search"></i></a>  </div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post web-design">
+<img alt="" src="upload/neuro.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Center for Neurological</h2>
+<span>Website Design</span>
+<div><a class="icon-action" href="upload/neuro-large.jpg"><i class="fa fa-search"></i></a> <a class="icon-action" href="http://www.neurocntr.com/" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post logo graphics-design">
+<img alt="" src="upload/las-americas.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Las Américas</h2>
+<span>Logotype Design</span>
+<div><a class="icon-action" href="upload/las-americas-large.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>
+
+<div class="project-post logo graphics-design">
+<img alt="" src="upload/3d-2.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>3D Logotype</h2>
+<span>3D Design</span>
+<div><a class="icon-action" href="upload/3d-2-large.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post web-design logo">
+<img alt="" src="upload/manexe_payment.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Manexe Advance Payment Solutions</h2>
+<span>Brand Design + Web Interface Development</span>
+<div><a class="icon-action" href="upload/manexe_payment_lg.jpg"><i class="fa fa-search"></i></a>
+<a href="http://www.manexe.com.do" target="_blank"><i class="fa fa-link"></i></a></div>
+</div>
+</div>
+</div>
+
+<div class="project-post graphics-design">
+<img alt="" src="upload/3d.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>3D Modeling Bedroom</h2>
+<span>3D Design</span>
+<div><a class="icon-action" href="upload/3d-large.jpg"><i class="fa fa-search"></i></a></div>
+</div>
+</div>
+</div>-->
+
+<!--<div class="project-post games">
+<img alt="" src="upload/games.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Physic Cannon</h2>
+<span>Mobile, PC, HTML5</span>
+
+<p><a href="http://www.agutz.com/Alien_Cannon" target="_blank" style="color:#FFF;">View Demo</a></p>
+
+</div>
+</div>
+</div>
+
+<div class="project-post games">
+<img alt="" src="upload/games-2.jpg">
+<div class="hover-box">
+<div class="project-title">
+<h2>Platformer Game Beta</h2>
+<span>Mobile, PC, HTML5</span>
+<p><a href="http://www.agutz.com/platformer-beta" target="_blank" style="color:#FFF;">View Demo</a></p>
+
+</div>
+</div>
+</div> -->
+
+</div>
+</div>
+</div>
+</div>
+<!-- End content -->
+
+</div>
+<!-- End Container -->
+
+<?php  include ('assets/info-box.php');?>
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.migrate.js"></script>
+<script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script type="text/javascript" src="js/jquery.imagesloaded.min.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.min.js"></script>
+<script type="text/javascript" src="js/retina-1.1.0.min.js"></script>
+<script type="text/javascript" src="js/jquery.nicescroll.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+
+</body>
+</html>
